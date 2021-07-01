5629bc0d15f3b69138af881b69909b22fd0f3ce0
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-01 23:55:38 +0530

    using switch statement for empwage

--- Empwage.java
+++ Empwage.java
@@ -1,5 +1,5 @@
 /**
-* This program is to add partTime employee and wage
+* This program is to solve using switch statement
 *
 * @author  Bathala Haresh
 */
@@ -9,30 +9,30 @@
     public static void main(String args[])
     {
         //constants
-        final int fullTime = 1;
-        final int partTime = 2;
+        final int partTime = 1;
+        final int fullTime = 2;
         final int wageHr = 20;
         
-        //random method to get values like 0 or 1 or 2
         int empType = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
+        int workingHours =0;
         
-
-        if (empType == fullTime)
+        //switcj case to find employee Present or absent or worked partTime
+        switch (empType)
         {
-            System.out.println("Employee is Present so ");
-            workingHours = 8;
-        } 
-        else if (empType == partTime) 
-        {
-            System.out.println("Employee has present PartTime so ");
-            workingHours = 4;
-        }else
-        {
-            System.out.println("Employee is Absent so ");
+            case fullTime:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case partTime:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");  
         }
-        //calculate the wage of an employee
+        //calculating wage of an employee
         int wage = workingHours * wageHr;
         System.out.println("Employee Daily Wage is " + wage);
+        
     }
 }
