3691e4b6324deb8f60d9dfe2cf64573a971a24d2
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-02 00:15:37 +0530

    calculating wages till maximum

--- Empwage.java
+++ Empwage.java
@@ -1,41 +1,50 @@
 /**
-* This program is to calculate wages for the month
+* This program is to calculate wages till a condition of total workingHours or days is reached for a month
 *
 * @author  Bathala Haresh
 */
 
-  public class Main
+  public class Empwage
 {
     public static void main(String args[])
     {
         //constants
         final int partTime = 1;
         final int fullTime = 2;
-        final int wageHr = 20;
-        final int workingDays = 20;
-    
+        final int WageHr = 20;
+        final int Max_Working_Days = 20;
+        final int Max_Working_Hrs = 100;
+
         int totalWage = 0;
+        int workingHrs = 0;
+        System.out.printf("%3s   %3s     %3s     %3s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
         
-        //loop that travels for day starting from 1 to 20 th day and calculating total wage
-        for (int day = 1; day <= workingDays; day++)
+        //for loop to check a condition working hours or days is reached maximum
+        for (int day = 1, totalWorkingHrs = 0; day <= Max_Working_Days
+                && totalWorkingHrs < Max_Working_Hrs; day++, totalWorkingHrs += workingHrs)
         {
+
             int empType = (int) (Math.random() * 100) % 3;
-            int workingHours =0;
             switch (empType)
             {
-                case fullTime:
-                    workingHours = 8;
-                    break;
-                case partTime:
-                    workingHours = 4;
-                    break;
-                default:
+            case fullTime:
+                workingHrs = 8;
+                break;
+            case  partTime:
+                workingHrs = 4;
+                break;
+            default:
+                workingHrs = 0;
+                break;
             }
-            int wage = workingHours * wageHr;
-            System.out.println("Day " + day + " wage is:" + wage);
+            //calculating totalwage for an employee
+            int wage = workingHrs * WageHr;
             totalWage += wage;
+            
+            //prints day ,workinghours,wage,totalhours and workinghours of an employee
+            System.out.printf("%3d  %3d %3d    %3d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+
         }
-        //prints the total wage for the month
-        System.out.println("Total wage for a month is " + totalWage);        
+        System.out.println("Total wage for the entire month is " + totalWage);
     }
 }
