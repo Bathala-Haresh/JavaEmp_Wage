519c947c7403d7740966d63cb9df681d31495de3
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-01 23:38:39 +0530

    employee-check

--- Empwage.java
+++ Empwage.java
@@ -8,6 +8,17 @@ class Empwage
 {
     public static void main(String args[])
     {
-        System.out.print("Welcome to Employee wage Computation program");
+        System.out.println("Attendence Checking ....");
+        
+        //Random function to get either 1 or 0
+        int employee=(int)(Math.random() * 100) % 2;
+       
+        if(employee == 1)
+        {
+            System.out.print("present");
+        }
+        else{
+            System.out.print("absent");
+        }
     }
 }
