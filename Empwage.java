5d2337eb0fb93e24a381f776c9b4a9e4aaa25846
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-01 23:34:57 +0530

    welcoming user to empwage problem

---
+++ Empwage.java
@@ -0,0 +1,13 @@
+/**
+* This program is to welcome user to Empwage problem
+*
+* @author  Bathala Haresh
+*/
+
+class Empwage
+{
+    public static void main(String args[])
+    {
+        System.out.print("Welcome to Employee wage Computation program");
+    }
+}
